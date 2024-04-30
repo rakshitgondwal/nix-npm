4212cc1b30ba1376b8ce06a18fbc675158be018b
Rakshit Gondwal <rakshitgondwal3@gmail.com>
2024-04-30 22:11:53 +0530

    fix
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>

--- buildNodeModules/flake.nix
+++ buildNodeModules/flake.nix
@@ -12,7 +12,6 @@
 
   outputs = { self, nixpkgs, ...} @ inputs:
     let
-      systems = ["x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin"];
       inherit (nixpkgs) lib;
       buildNodeModules = inputs.buildNodeModules.lib."x86_64-linux";
             callPackage = lib.callPackageWith ( {
