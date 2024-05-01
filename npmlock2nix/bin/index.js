ad3d232b1629b10f0c558d7550025ee31a2529cd
Rakshit Gondwal <rakshitgondwal3@gmail.com>
2024-05-01 22:30:28 +0530

    refactor
    Signed-off-by: Rakshit Gondwal <rakshitgondwal3@gmail.com>

---
+++ npmlock2nix/bin/index.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+// Importing the built-in 'console' module
+const { log } = console;
+
+// Function to print "Hello, World!"
+function helloWorld() {
+  log('Hello, World!');
+}
+
+// Call the function when the script is executed
+helloWorld();
+
